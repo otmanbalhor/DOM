c833e3da484bd41bbe321f90b245c0af864663a0
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-14 23:10:35 +0100

    Index.html script.js ex2

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,9 +1,24 @@
 
-element=document.querySelector('ol');
-element.setAttribute('id','maListe');
+let selectol=document.querySelector('ol');
+selectol.setAttribute('id','maListe');
 const liste = document.getElementById('maListe');
 
 const dernierenfant = liste.lastElementChild;
 
 liste.insertBefore(dernierenfant, liste.firstElementChild);
+
+
+const h2Section1 = document.querySelector('main section:nth-child(1) h2');
+const h2Section2 = document.querySelector('main section:nth-child(2) h2');
+const h2Section3 = document.querySelector('main section:nth-child(3) h2');
+
+
+const Content = h2Section2.innerHTML;
+
+
+h2Section2.innerHTML = h2Section3.innerHTML;
+
+
+h2Section3.innerHTML = Content;
+
 
