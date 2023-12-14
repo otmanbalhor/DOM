a2f9fa628a002ef7b08301365085b2bc3fa4b2cd
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-14 21:52:40 +0100

    Index.html script.js ex1

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,2 +1,9 @@
-/*
-*/
+
+element=document.querySelector('ol');
+element.setAttribute('id','maListe');
+const liste = document.getElementById('maListe');
+
+const dernierenfant = liste.lastElementChild;
+
+liste.insertBefore(dernierenfant, liste.firstElementChild);
+
