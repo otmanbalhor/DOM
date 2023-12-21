db64f87fc823288ed8ccf079739dbd3539ea45c5
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-21 22:52:13 +0100

    forms fin

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -9,29 +9,89 @@ let displayFN = document.getElementById('display-firstname');
 // Ajout d'un écouteur d'événement "keydown" sur le champ de saisie
 firstNameInput.addEventListener('keyup', function (event) {
 
-    // Vérifier si la touche pressée est la touche "Enter" (code clavier 13)
+    // Vérifie si la touche pressée est la touche "Enter" (code clavier 13)
     if (event.keyCode === 13) {
-        // Récupérer le contenu actuel du champ de saisie
+        // Récupére le contenu actuel du champ de saisie
         let inputValue = firstNameInput.value.trim();// Trim pour enlever les espaces avant et après
 
 
         if (inputValue !== "") {
-            // Ajouter la valeur actuelle dans le tableau
+            // Ajoute la valeur actuelle dans le tableau
             tab.push(inputValue);
 
-            // Mettre à jour le contenu du span avec le dernier mot saisi
+            // Met à jour le contenu du span avec le dernier mot saisi
             displayFN.textContent = tab;
 
             console.log(tab);// Afficher le tableau dans la console
 
         }
 
-        // Effacer le contenu du champ de saisie
+        // Efface le contenu du champ de saisie
         firstNameInput.value = "";
 
-        // Empêcher le comportement par défaut de la touche "Enter" (éviter le rechargement de la page)
+        // Empêche le comportement par défaut de la touche "Enter" (évite le rechargement de la page)
         event.preventDefault();
     }
 });
 
+let ageInput = document.getElementById('age');
+let hardTruthSection = document.getElementById('a-hard-truth');
 
+
+ageInput.addEventListener('keyup', function (event) {
+
+
+    let age = parseInt(event.target.value);
+
+    if (event.keyCode === 13) {
+        age = ageInput.value.trim();
+        ageInput.value = "";
+        event.preventDefault();
+    }
+
+    else if (age >= 18) {
+
+        hardTruthSection.style.visibility = 'visible';
+
+    } else {
+
+        hardTruthSection.style.visiblity = 'hidden';
+
+    }
+
+});
+
+let passwordInput = document.getElementById('pwd');
+let confPasswordInput = document.getElementById('pwd-confirm');
+
+passwordInput.addEventListener('keyup', validatePassword);
+confPasswordInput.addEventListener('keyup', validatePassword);
+
+function validatePassword() {
+    var password = passwordInput.value;
+    var confPassword = confPasswordInput.value;
+
+    if (password.length < 6 || password === confPassword) {
+        passwordInput.style.border="3px solid green";
+        confPasswordInput.style.border="3px solid green";
+    } else {
+        passwordInput.style.border="3px solid red";
+        confPasswordInput.style.border="3px solid red";
+    }
+}
+
+let darkmode = document.getElementById('toggle-darkmode');
+
+darkmode.addEventListener('change',function(){
+    let alternMode = darkmode.value;
+
+    if(alternMode=== 'dark'){
+        document.body.style.backgroundColor = '#000000';
+        document.body.style.color = 'white';
+    }
+    else{
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = '#000000';
+    }
+  
+});
