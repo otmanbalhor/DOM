69d9c2dd1e1c1bd9d4e7dc62bfd551b8662d019b
otman <otman@DESKTOP-4RA473J.localdomain>
2023-12-21 14:40:27 +0100

    events ex01

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -1,2 +1,37 @@
-/*
-*/
+let tab = [];// Initialisation du tableau vide
+
+// Sélection du premier champ de saisie
+let firstNameInput = document.getElementById('firstname');
+
+// Sélection du span où afficher le prénom
+let displayFN = document.getElementById('display-firstname');
+
+// Ajout d'un écouteur d'événement "keydown" sur le champ de saisie
+firstNameInput.addEventListener('keyup', function (event) {
+
+    // Vérifier si la touche pressée est la touche "Enter" (code clavier 13)
+    if (event.keyCode === 13) {
+        // Récupérer le contenu actuel du champ de saisie
+        let inputValue = firstNameInput.value.trim();// Trim pour enlever les espaces avant et après
+
+
+        if (inputValue !== "") {
+            // Ajouter la valeur actuelle dans le tableau
+            tab.push(inputValue);
+
+            // Mettre à jour le contenu du span avec le dernier mot saisi
+            displayFN.textContent = tab;
+
+            console.log(tab);// Afficher le tableau dans la console
+
+        }
+
+        // Effacer le contenu du champ de saisie
+        firstNameInput.value = "";
+
+        // Empêcher le comportement par défaut de la touche "Enter" (éviter le rechargement de la page)
+        event.preventDefault();
+    }
+});
+
+
